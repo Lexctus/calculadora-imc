c203311d22c8464d8fe4f68d7c4a347403401943
Alex dos Santos <allexnascimento@gmail.com>
2022-08-08 22:12:27 -0300

    Correção de erro no Input, mudança no estilo e adição mensagem personalizada no resultado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // CAPTURAR EVENTO DE SUBMIT DO FORMULÁRIO
 const form = document.querySelector('#formulario');
 
+//FUNÇÃO QUE CAPTURA OS DADOS E RETORNA O RESULTADO
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     // console.log('Evento Prevenido');
@@ -13,17 +14,61 @@ form.addEventListener('submit', function (e) {
     
     // Validar se os valores são Numbers
     if (!peso) {
+        
         setResultado('Peso inválido', false);
         return;
     }
 
     if (!altura) {
+        
         setResultado('Altura inválida', false);
         return;
     }
 
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+
+    console.log('Imc:',imc,nivelImc);
 });
 
+
+// FUNÇÃO QUE CLASSIFICA O PESO DO USUÁRIO
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso','Obesidade Grau I', 'Obesidade Grau II','Obesidade Grau III'];
+
+    if (imc >= 39.9) {
+        return nivel[5];
+    }
+    if (imc >= 34.9) {
+        return nivel[4];
+    }
+    if (imc >= 29.9) {
+        return nivel[3];
+    }
+    if (imc >= 24.9) {
+        return nivel[2];
+    }
+    if (imc >= 18.5) {
+        return nivel[1];
+    }
+    if (imc < 18.5) {
+        return nivel[0];
+    }
+}
+
+//FUNÇÃO QUE CALCULA O IMC
+function getImc (peso, altura) {
+   
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+    
+}
+
+//FUNÇÃO QUE CRIA UM PARAGRÁFO HTML
 function paragrafo (className) {
     
     const p = document.createElement('p');
@@ -33,20 +78,14 @@ function paragrafo (className) {
 function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
-    // p.innerHTML = 'Qualquer coisa';
-    // resultado.appendChild(p);
+
     const p = paragrafo();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
     p.innerHTML = msg;
     resultado.appendChild(p);
 }
-
-
-// Dados
-const peso = 65;
-const altura = 1.70;
-
-// Processamento
-
-const imc = peso/(altura*altura);
-
-console.log(imc);
