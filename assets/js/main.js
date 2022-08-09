72b61e3111b168d0d2838a2ccfa8f59965fc073e
Alex dos Santos <allexnascimento@gmail.com>
2022-08-08 21:10:25 -0300

    Criada funções para validar valores e mostrar resultado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,52 @@
-alert(1);
+// CAPTURAR EVENTO DE SUBMIT DO FORMULÁRIO
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // console.log('Evento Prevenido');
+    // setResultado('O resultado aparecerá aqui!');
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    
+    // Validar se os valores são Numbers
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+});
+
+function paragrafo (className) {
+    
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    // p.innerHTML = 'Qualquer coisa';
+    // resultado.appendChild(p);
+    const p = paragrafo();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
+
+
+// Dados
+const peso = 65;
+const altura = 1.70;
+
+// Processamento
+
+const imc = peso/(altura*altura);
+
+console.log(imc);
